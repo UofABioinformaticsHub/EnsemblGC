02a3599545746719456ac6b2d3546260e57acda9
baerlachlan <baerlachlan@gmail.com>
2019-11-15 16:28:27 +1030

    Initial commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Ensembl_GC
+
+<p>This repository holds precalculated GC content for select species from multiple ensembl releases.<br>
+All RDS files contain the GC content and length for the transcripts as defined at time of release, as GRanges objects.<br>
+Links are provided below.</p>
+
+## Ensembl 98
+Last generated 15 November, 2019.
+- [Bos taurus](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Bos_taurus.ARS-UCD1.2.98.rds)
+- [Danio rerio](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Danio_rerio.GRCz11.98.rds)
+- [Gallus gallus](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Gallus_gallus.GRCg6a.98.rds)
+- [Homo sapiens](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Homo_sapiens.GRCh38.98.rds)
+- [Mus musculus](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Mus_musculus.GRCm38.98.rds)
+- [Rattus norvegicus](https://github.com/baerlachlan/Ensembl_GC/tree/master/Release98/Rattus_norvegicus.Rnor_6.0.98.rds)
+
+## Ensembl 97
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 96
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 95
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 94
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 93
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 92
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 91
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
+
+## Ensembl 90
+Last generated 15 November, 2019.
+- Bos taurus: 
+- Danio rerio:
+- Gallus gallus:
+- Homo sapiens:
+- Mus musculus:
+- Rattus norvegicus:
---
+++ Release96/GenerateGC.R
@@ -0,0 +1,86 @@
+#' Generate the GC Content for:
+#' - Bos taurus
+#' - Danio rerio
+#' - Gallus gallus
+#' - Homo sapiens
+#' - Mus musculus
+#' - Rattus norvegicus
+#' 
+#' All transcriptomes use the primary genome build for Ensembl release 96
+#' 
+
+library(dplyr)
+library(tidyverse)
+library(Biostrings)
+library(AnnotationHub)
+library(here)
+
+setwd(here::here("Release96"))
+
+spList <- list(
+  Bs = c(sp = "Bos_taurus", bld = "ARS-UCD1.2", rls = "96"),
+  Dr = c(sp = "Danio_rerio", bld = "GRCz11", rls = "96"),
+  Gg = c(sp = "Gallus_gallus", bld = "GRCg6a", rls = "96"),
+  Hs = c(sp = "Homo_sapiens", bld = "GRCh38", rls = "96"),
+  Mm = c(sp = "Mus_musculus", bld = "GRCm38", rls = "96"),
+  Rn = c(sp = "Rattus_norvegicus", bld = "Rnor_6.0", rls = "96")
+)
+
+getGCLen <- function(x) {
+  sp <- x[1]
+  bld <- x[2]
+  rls <- x[3]
+  url <- paste0(
+    "ftp://ftp.ensembl.org/pub/release-", 
+    rls, 
+    "/fasta/", 
+    str_to_lower(sp), 
+    "/cdna/", 
+    sp,
+    ".",
+    bld, 
+    ".cdna.all.fa.gz"
+  )
+  download.file(url, file.path(tempdir(), "temp.fa"))
+  tr <- file.path(tempdir(), "temp.fa") %>% readDNAStringSet()
+  df <- tibble(
+    id = names(tr), 
+    length = width(tr), 
+    gc = rowSums(alphabetFrequency(tr, baseOnly = TRUE)[,c("G", "C")]) / length
+  ) %>% 
+    mutate(
+      tx_id = str_extract(id, "ENS[A-Z]+[0-9]+"),
+      gene_id = str_extract(id, "ENS[A-Z]*G[0-9]+"),
+      location = str_extract(id, paste0("[a-z]+:", bld, ":.+:[0-9]+:[0-9]+:-?[0-9]+")),
+      location = str_remove(location, paste0("[a-z]+:", bld, ":")),
+      gene_symbol =  str_extract(id, "gene_symbol:.+ desc"),
+      gene_symbol = str_remove(gene_symbol, "gene_symbol:"),
+      gene_symbol = str_remove(gene_symbol, " desc")
+    ) %>%
+    dplyr::select(-id) %>%
+    dplyr::arrange(tx_id, desc(length)) %>%
+    separate("location", c("seqnames", "start", "end", "strand"), sep = ":") %>%
+    mutate(strand = ifelse(strand == 1, "+", "-")) %>%
+    as.data.frame() %>%
+    column_to_rownames("tx_id")
+  ah <- AnnotationHub()
+  ahRls <- ah %>%
+    subset(species == str_replace(sp, "_", " ")) %>%
+    subset(dataprovider == "Ensembl") %>%
+    subset(rdataclass == "EnsDb")
+  ens <- ah[[
+    ahRls$ah_id[
+      ahRls$title == grep(
+        paste0("Ensembl ", rls), ahRls$title, value = TRUE
+      )
+      ]
+    ]]
+  gr <- makeGRangesFromDataFrame(
+    df, 
+    seqinfo = seqinfo(ens),
+    keep.extra.columns = TRUE
+  )
+  saveRDS(gr, paste0(sp, ".", bld, ".", rls, ".rds"))
+}
+
+lapply(spList, getGCLen)
